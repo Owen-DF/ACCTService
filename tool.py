c8d308295e2c8518f47dc52ed00233e70cf31bcd
Owen Fleischer <ofleischer@metroelectrical.com>
2024-06-27 08:58:24 -0400

    adding tool.py

--- tool.py
+++ tool.py
@@ -1,3 +1,28 @@
-import tkinter as tk
+from tkinter import *
 
 
+
+def submitButton(button):
+    
+
+    return
+
+
+
+
+root = Tk()
+w = Label(
+    root, 
+    text = "Accounting Tool",
+    font = ("Helvetica", 24 ))
+w.pack()
+
+Dinput = Entry(root)
+Dinput.pack()
+
+DButton = Button(root, text = "submit directory", command = submitButton(Dinput))
+DButton.pack()
+
+
+root.geometry('500x500')
+root.mainloop()
