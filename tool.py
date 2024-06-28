f708dc8a9155b60b7e33595dbde1ef127922c0c7
Owen Fleischer <ofleischer@metroelectrical.com>
2024-06-28 14:38:17 -0400

    all of logic is in the file, working on file paths now

--- tool.py
+++ tool.py
@@ -8,11 +8,16 @@ import openpyxl
 import warnings
 import glob
 import time
-
+ 
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+ 
 
 #file paths for reference, change here for future file reconstructing
-YEARLY_COSTING = r"Z:\Accounting\13 Python Reports\Yearly Costing.xlsx"
-
+YEARLY_COSTING = r"C:\Users\ofleischer\OneDrive - MetroTek Electrical\Coding\SampleFiles\Yearly Costing.xlsx"  #have
+BLANK_CELL = 'G101'
+OPENPOS = r'Z:\Accounting\13 Python Reports\Open POs.xlsx'   #need
+JOBHOURS = r'Z:\Accounting\13 Python Reports\Job Hours.xlsx' #need
 
 
 #functions
@@ -66,26 +71,21 @@ def fileNav(filePath, itemList):
 
 def pullExcel(matchingFolders, itemList):
     ExcelFilePaths = []
-
     if not matchingFolders:
-        print("No folders to process")
+      #  print("No folders to process")
         return
-
     try:
         stepValue = 100 / len(matchingFolders)
     except ZeroDivisionError:
-        print("Cannot divide by zero, most likely no input in form")
+      #  print("Cannot divide by zero, most likely no input in form")
         return
-
     for folder in matchingFolders:
         excelFiles = []
         pattern = os.path.join(folder, '*.xlsm')  #these excel files are not xlsx, whoever decided that tbere are different types of excel files deserve something bad, i am cranky
-        print(f"Searching for Excel files with pattern: {pattern}")
-        
+       # print(f"Searching for Excel files with pattern: {pattern}")       
         for file in glob.glob(pattern):
             excelFiles.append(file)
-            print(f"Found Excel File: {file}")
-
+           # print(f"Found Excel File: {file}")
         if excelFiles:
             if len(excelFiles) > 1:
                 mostRecent = max(excelFiles, key=os.path.getatime)
@@ -93,23 +93,23 @@ def pullExcel(matchingFolders, itemList):
             else:
                 ExcelFilePaths.append(excelFiles[0])
         else:
-            print(f"No Excel files found in folder: {folder}")
-        
+            return
         bar['value'] += stepValue
         app.update_idletasks()
 
-    processExcelFiles(ExcelFilePaths, itemList)
+    PreProcessExcelFiles(ExcelFilePaths, itemList)
 
 
-def processExcelFiles(ExcelList, itemList):
+def PreProcessExcelFiles(ExcelList, itemList):
+    df = pd.concat([pd.read_excel(YEARLY_COSTING, sheet_name=sheet_name) for sheet_name in ['2024','2023', '2022', '2021', '2020', '2019', '2018']])
+    df.fillna('', inplace=True)
+    
 
-    print(ExcelList)
-    print(itemList)
+    for i, file_path in enumerate(ExcelList):
+        searchTerm = itemList[i]
+        processExcel(file_path, searchTerm, df)
 
-
-
-
-
+def processExcel(filePath, searchTerm, df):
     items = {
         '01.01': 'G9', '01.02': 'G10', '02.01': 'G21', '02.02': 'G22', '02.03': 'G23',
         '03.01': 'G37', '03.02': 'G38', '03.03': 'G39', '04.01': 'G48', '04.02': 'G49',
@@ -118,8 +118,43 @@ def processExcelFiles(ExcelList, itemList):
         '06.05': 'G82', '07.0': 'G90', '08.01': 'G94', '08.02': 'G95', '08.03': 'G96','08.04': 'G97'
     }
     
+    workbook = openpyxl.load_workbook(filePath, read_only=False, keep_vba = True)
+    worksheet = workbook['JC']
+
+    for item, cell in items.items():
+        result = df.loc[(df['Name'].astype(str).str.contains(searchTerm)) & (df['Item'].astype(str).str.contains(item)), 'Amount'].sum()
+        worksheet.save(filePath)
+    workbook.save(filePath)
 
 
+    result = df.loc[(df['Name'].astype(str).str.contains(searchTerm))&(df['Item'] == ''), 'Amount'].sum()
+    cell = BLANK_CELL
+    worksheet[cell] = result
+    workbook.save(filePath)
+
+    df_open_pos = pd.read_excel(OPENPOS)
+    items = {
+        '01.01': 'I9', '01.02': 'I10', '02.01': 'I21', '02.02': 'I22', '02.03': 'I23',
+        '03.01': 'I37', '03.02': 'I38', '03.03': 'I39', '04.01': 'I48', '04.02': 'I49',
+        '04.03': 'I50', '04.04': 'I51', '05.01': 'I63', '05.02': 'I64', '05.03': 'I65',
+        '05.04': 'I66', '06.01': 'I78', '06.02': 'I79', '06.03': 'I80', '06.04': 'I81',
+        '06.05': 'I82', '07.0': 'I90', '08.01': 'I94', '08.02': 'I95', '08.03': 'I96','08.04': 'I97'
+    }
+
+    workbook = openpyxl.load_workbook(filePath, read_only = False, keep_vba = True)
+    worksheet = workbook['JC']
+
+    for item, cell in items.items():
+        result = df_open_pos.loc[(df_open_pos['Name'].astype(str).str.contains(searchTerm)) & (df_open_pos['Item'].astype(str).str.contains(item)), 'Open Balance'].sum()
+        worksheet[cell] = result
+    workbook.save(filePath)
+
+    dfJobHours = pd.read_excel(JOBHOURS)
+    totalAmount = dfJobHours.loc[dfJobHours['Job Description'].astyhpe(str).str.contains(searchTerm), 'Hours'].sum()
+
+    cell = 'G87'
+    worksheet[cell] = totalAmount
+    workbook.save(filePath)
     
     return
 
